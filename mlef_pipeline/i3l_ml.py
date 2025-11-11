03687c84e544ccf81cd5c5da76c9e2a8b99503d6
Alberto Ferrarin <48485252+Ferraa96@users.noreply.github.com>
2025-11-11 18:15:54 +0100

    Fixed prediction saving for classification

--- mlef_pipeline/i3l_ml.py
+++ mlef_pipeline/i3l_ml.py
@@ -177,7 +177,7 @@ class ML:
             index=y.index
         )
 
-        fm_rad_features = [col for col in X.columns if col.startswith('pred_')]
+        fm_rad_features = [col for col in X.columns if col.startswith('pred_') or col.startswith('log_pred_')]
         if len(fm_rad_features) > 0:
             selected_fm_rad_features = self.lasso_selection(
                 X[fm_rad_features], 
@@ -186,7 +186,7 @@ class ML:
                 tolerance=10
             )
             X = X.drop(columns=[col for col in fm_rad_features if col not in selected_fm_rad_features])
-            
+
         if select_features:
             selected_features = self.lasso_selection(X, y, target_features=15, tolerance=10)
             X = X[selected_features]
