a7358466e320ee6e457241cafac216bc3af7b161
Alberto Ferrarin <48485252+Ferraa96@users.noreply.github.com>
2025-11-27 12:26:28 +0100

    Fixed warnings for fairness plots

--- utils/helper_classification.py
+++ utils/helper_classification.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import os
 from .DeLong_test import auc_roc_ci, delong_roc_variance, fastDeLong_no_weights, compute_ground_truth_statistics
 from scipy import stats
@@ -1761,6 +1762,8 @@ def plot_fairness_by_center(
     if patients_per_outcome is not None:
         label_offset = 0.02
         for container in ax.containers:
+            if not all(isinstance(c, mpatches.Rectangle) for c in container):
+                continue
             try:
                 if len(container.patches) == 0:
                     continue
@@ -1933,6 +1936,8 @@ def plot_fairness_by_group(
     if patients_per_outcome is not None:
         label_offset = 0.02
         for container in ax.containers:
+            if not all(isinstance(c, mpatches.Rectangle) for c in container):
+                continue
             try:
                 if len(container.patches) == 0:
                     continue
